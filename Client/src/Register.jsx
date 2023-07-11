a27e7d8b62446678685ca563a2525ac5b75aeec8
Iamtheusername112 <apkfem@gmail.com>
2023-07-11 07:42:53 +0200

    first commit

---
+++ Client/src/Register.jsx
@@ -0,0 +1,21 @@
+export default function Rrgister() {
+  return (
+    <div className="bg-blue-50 h-screen flex items-center">
+      <form className="w-64 mx-auto">
+        <input
+          type="text"
+          placeholder="username"
+          className="block w-full rounded-sm p-2 mb-2 border"
+        />
+        <input
+          type="text"
+          placeholder="password"
+          className="block w-full rounded-sm p-2 mb-2 border"
+        />
+        <button className="bg-blue-500 text-white block w-full rounded-sm p-2">
+          Register
+        </button>
+      </form>
+    </div>
+  );
+}
